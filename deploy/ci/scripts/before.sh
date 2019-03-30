c8fa68b86730c5ae9fde3548e73a0069873c3ffc
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2019-03-30 15:56:31 -0400

    Add preliminary Travis CI support.

---
+++ deploy/ci/scripts/before.sh
@@ -0,0 +1,5 @@
+    
+#!/bin/bash
+echo Downloading modpack build tools
+wget https://raw.githubusercontent.com/andrewjmetzger/foxcraft.mtz.gr/master/deploy/ci/scripts/launcher-build-all.jar
+echo Downloaded latest version of build tools, prepare for modpack building
---
+++ deploy/ci/scripts/main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo Hopefully, build.sh has been run before this script can take effect.
+echo If not, the CI has messed up.
+
+for d in */
+do
+    if [ -e nobuild.txt ]
+    then
+        echo "Could not build directory, not permitted."
+    else
+        echo "Attempting build..."
+        safed=${d%"."}
+        number=$RANDOM
+        cd "$d"
+        ls
+        java -jar ../build.jar --version "$number" --input . --output ../upload --manifest-dest "../upload/$safed.json"
+        cd ..
+        echo "Building directory attempted to complete"
+    fi
+done
